24f5e37700173d6c26a4af1da010da76b466bd59
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-18 22:58:19 +0530

    Modify index.html,  index.js, Add testing.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import reactDom from 'react-dom';
-import ReactDOM from 'react-dom';
+import reactDom from 'react-dom'
 
-function Greeting(){
-  return <h4>this is abhishek and this is my first component</h4>;
+function Greeting() {
+  return (
+    <div>
+      <h4>Hello World</h4>
+      <ul>
+        <li>
+          <a href='#'>Hello World </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
-reactDom.render(<Greeting/>, document.getElementById('root'));
+// This is happening in the backend
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'Hi World')
+//   )
+// }
+
+reactDom.render(<Greeting />, document.getElementById('root'))
