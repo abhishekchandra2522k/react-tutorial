c104c1734a50446c37a0efda4e6065be19ad454f
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-23 22:48:38 +0530

    Add props, agruments / parameters for different value with same component

--- src/index.js
+++ src/index.js
@@ -3,27 +3,40 @@ import reactDom from 'react-dom'
 
 import './index.css'
 
+const firstBook = {
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg',
+  title: 'Ikigai: The Japanese secret to a long and Happy Life',
+  author: 'Héctor García',
+}
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/I/518bm5DN-VS._AC_SX184_.jpg',
+  title: 'Malibu Rising : A Novel',
+  author: 'Taylor Jenkins Reid',
+}
+
 function BookList() {
   return (
     <section className='BookList'>
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   )
 }
-const author = 'Héctor García'
-const Book = () => {
-  const title = 'Ikigai: The Japanese secret to a long and Happy Life'
+const Book = (props) => {
+  console.log(props)
   return (
     <article className='book'>
-      <img
-        src='https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg'
-        alt=''
-      />
-      <h1>{title}</h1>
-
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x = 4}</p> */}
-      <p>{6 + 6}</p>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
