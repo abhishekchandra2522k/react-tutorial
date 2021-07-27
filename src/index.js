84f7def8df1b3010f308e213049074a34401a6f0
abhishekchandra2522k <1ac23456789@gmail.com>
2021-07-27 22:56:38 +0530

    Add array of objects to iterate over them rather than passing each object sepeartely (using map method in javascript we can iterate over array of objects)

--- src/index.js
+++ src/index.js
@@ -2,50 +2,40 @@ import React from 'react'
 import reactDom from 'react-dom'
 
 import './index.css'
-
-const firstBook = {
-  img: 'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg',
-  title: 'Ikigai: The Japanese secret to a long and Happy Life',
-  author: 'Héctor García',
-}
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/518bm5DN-VS._AC_SX184_.jpg',
-  title: 'Malibu Rising : A Novel',
-  author: 'Taylor Jenkins Reid',
-}
+const books = [
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg',
+    title: 'Ikigai: The Japanese secret to a long and Happy Life',
+    author: 'Héctor García',
+  },
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/71aFt4%2BOTOL._AC_UL200_SR200,200_.jpg',
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+  },
+  {
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg',
+    title: 'Atomic Habits',
+    author: 'James Clear',
+  },
+]
 
 function BookList() {
   return (
     <section className='BookList'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione in
-          quas minus, asperiores illum temporibus est. Veniam fuga corrupti
-          incidunt in, vel dolores esse unde enim voluptatum officia. Mollitia,
-          eligendi.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book book={book} />
+      })}
     </section>
   )
 }
 const Book = (props) => {
-  const { img, title, author } = props
-  console.log(props)
+  const { img, title, author } = props.book
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   )
 }
